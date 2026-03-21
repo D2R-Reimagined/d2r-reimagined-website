3e791ce7f64dfbde24c637c3b9ac5af45f35ed7c
Delegus <leminkainen118@gmail.com>
2026-03-21 05:24:04 -0500

    Update damage-types.ts

--- src/utilities/damage-types.ts
+++ src/utilities/damage-types.ts
@@ -25,7 +25,7 @@ export interface IUniqueItem {
 export function getDamageTypeString(type: number): string {
     switch (type) {
         case 4: return 'Elemental Damage:';
-        case 3: return 'One-Hand Damage:';
+        case 3: return 'Damage:';
         case 2: return 'Throw Damage:';
         case 1: return 'Two-Hand Damage:';
         case 0: return 'One-Hand Damage:';
