45472e1ea83c68bb5e901e2c7aec143e73f5761f
Collin Herber <rustaddicts@gmail.com>
2026-08-27 10:08:03 -0500

    feat: add discord link to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,6 +2,7 @@
   <div class="mx-auto flex max-w-7xl flex-col gap-3 px-5 py-8 text-sm text-parchment-300 sm:flex-row sm:items-center sm:justify-between">
     <p>Community-built tools for the D2R Reimagined mod.</p>
     <div class="flex gap-5">
+      <a class="hover:text-ember-400" href="https://discord.gg/ZvQD4MARxz" target="_blank" rel="noreferrer">Discord</a>
       <a class="hover:text-ember-400" href="https://github.com/D2R-Reimagined" target="_blank" rel="noreferrer">GitHub</a>
       <a class="hover:text-ember-400" href="https://www.nexusmods.com/diablo2resurrected/mods/503" target="_blank" rel="noreferrer">Nexus Mods</a>
       <a class="hover:text-ember-400" href="https://wiki.d2r-reimagined.com/" target="_blank" rel="noreferrer">Wiki</a>
